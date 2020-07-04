bde8a72ab96f720f1fbd91920a50a03188ed818a
lip <lip576@outlook.com>
2020-07-04 21:23:09 +0800

    Add the first activity project
    Signed-off-by: lip <lip576@outlook.com>

---
+++ ActivityFirst/app/src/main/java/com/example/activityfirst/DialogActivity.java
@@ -0,0 +1,13 @@
+package com.example.activityfirst;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DialogActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.dialog_layout);
+    }
+}
