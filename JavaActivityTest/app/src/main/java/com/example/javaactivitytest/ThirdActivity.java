d94d91f4c8f214159c44d8d449caeb072fb3a048
lip <lip576@outlook.com>
2020-07-06 23:08:19 +0800

    singleinstance 模式 并在 ThirdActivity 中启动 FirstActivity
    Signed-off-by: lip <lip576@outlook.com>

--- JavaActivityTest/app/src/main/java/com/example/javaactivitytest/ThirdActivity.java
+++ JavaActivityTest/app/src/main/java/com/example/javaactivitytest/ThirdActivity.java
@@ -8,18 +8,18 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
-public class SecondActivity extends AppCompatActivity {
+public class ThirdActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         Log.d("SecondActivity", "Task id is" + getTaskId());
-        setContentView(R.layout.second_layout);
-        Button button2 = (Button) findViewById(R.id.button_2);
-        button2.setOnClickListener(new View.OnClickListener() {
+        setContentView(R.layout.third_layout);
+        Button button3 = (Button) findViewById(R.id.button_3);
+        button3.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(SecondActivity.this, ThirdActivity.class);
+                Intent intent = new Intent(ThirdActivity.this, FirstActivity.class);
                 startActivity(intent);
             }
         });
