7e6291831826fbffa176ca03000c6b0f46f7fb1e
lip <lip576@outlook.com>
2020-07-09 22:22:06 +0800

    实现接口的方式进行注册监听器
    Signed-off-by: lip <lip576@outlook.com>

--- UIWidgetTest/app/src/main/java/com/example/uiwidgettest/MainActivity.java
+++ UIWidgetTest/app/src/main/java/com/example/uiwidgettest/MainActivity.java
@@ -6,24 +6,22 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Button button = (Button) findViewById(R.id.button);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                //switch (view.getId()) {
-                //    case R.id.button:
-                //        break;
-                //    default:
-                //        break;
-                //}
-                    //在此加入逻辑
-            }
-        });
+        button.setOnClickListener((View.OnClickListener) this);
+    }
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.button:
+                break;
+                default:
+                    break;
+        }
     }
 }
