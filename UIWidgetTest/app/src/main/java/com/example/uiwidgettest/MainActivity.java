b52841685c2dd59ff3641fcb023fa58a8bed9fd6
lip <lip576@outlook.com>
2020-07-12 22:43:10 +0800

    通过代码动态地更改 ImageView 中的图片
    Signed-off-by: lip <lip576@outlook.com>

--- UIWidgetTest/app/src/main/java/com/example/uiwidgettest/MainActivity.java
+++ UIWidgetTest/app/src/main/java/com/example/uiwidgettest/MainActivity.java
@@ -6,26 +6,29 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
     private EditText editText;
+    private ImageView imageView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Button button = (Button) findViewById(R.id.button);
         editText = (EditText) findViewById(R.id.edit_text);
+        imageView = (ImageView) findViewById(R.id.image_view);
         button.setOnClickListener((View.OnClickListener) this);
     }
     @Override
     public void onClick(View v) {
         switch (v.getId()) {
             case R.id.button:
-                String inputText = editText.getText().toString();
-                Toast.makeText(MainActivity.this, inputText,
-                        Toast.LENGTH_SHORT).show();
+                //String inputText = editText.getText().toString();
+                //Toast.makeText(MainActivity.this, inputText, Toast.LENGTH_SHORT).show();
+                imageView.setImageResource(R.drawable.img_2);
                 break;
                 default:
                     break;
